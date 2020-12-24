4232708cf663d417026b81742a359bbe7056de6d
Jee <jee@jeer.fr>
2020-12-24 11:18:33 +0100

    run

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 echo "start mirroring this repo"
 
-GIT_FULL_URL="https://${GIT_MIRROR_USER}:${GIT_MIRROR_TOKEN}@${REMOTE_MIRROR_REPO}"
-echo ${GIT_FULL_URL}
+GIT_FULL_URL="https://$GIT_MIRROR_USER:$GIT_MIRROR_TOKEN@$REMOTE_MIRROR_REPO"
+echo $GIT_FULL_URL
 
 git checkout ${DRONE_BRANCH}
-git push ${GIT_FULL_URL} ${DRONE_BRANCH}
+git push $GIT_FULL_URL $DRONE_BRANCH
 
 echo "start mirroring this repo" 
