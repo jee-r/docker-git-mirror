50746afc725bc92538c429ed6adba77928fe797c
Jee <jee@jeer.fr>
2020-12-24 12:18:25 +0100

    more logic and output in script

--- run.sh
+++ run.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-echo "start mirroring this repo"
-
-GIT_FULL_URL="https://$GIT_MIRROR_USER:$GIT_MIRROR_TOKEN@$REMOTE_MIRROR_REPO"
-echo $GIT_FULL_URL
-
+#CHECKOUT
+echo "Checkout in commited branch"
 git checkout ${DRONE_BRANCH}
-git push $GIT_FULL_URL $DRONE_BRANCH
 
-echo "start mirroring this repo" 
+#ENV
+echo "check if env is set"
+if [ -z $GIT_MIRROR_USER ]; then
+    echo "GIT_MIRROR_USER is set : $GIT_MIRROR_USER"
+else
+    echo "[ERROR] GIT_MIRROR_USER is not set"
+fi
+
+if [ -z $GIT_MIRROR_REPO ]; then
+    echo "GIT_MIRROR_REPO is set : $GIT_MIRROR_USER"
+else
+    echo "[ERROR] GIT_MIRROR_REPO is not set"
+fi
+
+if [ -z $GIT_MIRROR_TOKEN ]; then
+    echo "mirror token is set (protected): $GIT_MIRROR_USER"
+else
+    echo "[ERROR] GIT_MIRROR_TOKEN is not set"
+fi
+
+#PUSH
+echo "start mirroring this repo ..."
+git push https://$GIT_MIRROR_USER:$GIT_MIRROR_TOKEN@$REMOTE_MIRROR_REPO $DRONE_BRANCH
+
+exit_code=$?
+if [ $exit_code != 0 ]; then
+    echo "[ERROR] Mirroring failed ($exit_code)"
+    exit 1
+else
+    echo "Mirroring success"
+    exit 0
+fi
+
+exit 1
